b1f91d175d481b379089c2af2bfcf762af17a366
Andy Christiansen <andy.christ79@gmail.com>
2026-06-01 16:00:09 +0000

    feat: Add BookRef to Careers

--- src/data/careers.ts
+++ src/data/careers.ts
@@ -1,9 +1,11 @@
 import { HeroPath } from '../types/hero';
+import { BookRef } from '../types/bookRef';
 
 export interface Career {
     name: string;
     path: HeroPath;
     abilities: string[];
+    bookRef?: BookRef;
 }
 
 export const CAREERS: Career[] = [
@@ -11,67 +13,142 @@ export const CAREERS: Career[] = [
     {
         name: 'Gladiator',
         path: 'Warrior',
-        abilities: ['Blood Rage', 'Head Butt']
+        abilities: ['Blood Rage', 'Head Butt'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 307
+        }
     }, {
         name: 'Berserker',
         path: 'Warrior',
-        abilities: ['Seeing Red', 'Raining Blows']
+        abilities: ['Seeing Red', 'Raining Blows'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 398
+        }
     }, {
         name: 'Ranger',
         path: 'Warrior',
-        abilities: ['Lay of the Land', 'Nature\'s Revenge']
+        abilities: ['Lay of the Land', 'Nature's Revenge'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 417
+        }
     }, {
         name: 'Cavalier',
         path: 'Warrior',
-        abilities: ['Shield Spind', 'Shield Wall']
+        abilities: ['Shield Spind', 'Shield Wall'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 634
+        }
     }, {
         name: 'Shadow Ranger',
         path: 'Warrior',
-        abilities: ['Black Rain', 'Thorn Fist']
+        abilities: ['Black Rain', 'Thorn Fist'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 814
+        }
     }, {
         name: 'Inquisitor',
         path: 'Warrior',
-        abilities: ['Cleansing Light', 'Avenging Spirit']
+        abilities: ['Cleansing Light', 'Avenging Spirit'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 668
+        }
     },
     // Mage Careers
     {
         name: 'Alchemist',
         path: 'Mage',
-        abilities: ['Good Taste', 'Midas Touch']
+        abilities: ['Good Taste', 'Midas Touch'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 401
+        }
     }, {
         name: 'Pyromancer',
         path: 'Mage',
-        abilities: ['Ignite', 'Burn']
+        abilities: ['Ignite', 'Burn'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 407
+        }
     }, {
         name: 'Medic',
         path: 'Mage',
-        abilities: ['Mend', 'Torniquet']
+        abilities: ['Mend', 'Torniquet'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 680
+        }
     }, {
         name: 'Icelock',
         path: 'Mage',
-        abilities: ['Ice Shards', 'Ice Shield']
+        abilities: ['Ice Shards', 'Ice Shield'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 761
+        }
     }, {
         name: 'Necromancer',
         path: 'Mage',
-        abilities: ['Shades', 'Sacrifice']
+        abilities: ['Shades', 'Sacrifice'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 850
+        }
     },
     // Rogue Careers
     {
         name: 'Pickpocket',
         path: 'Rogue',
-        abilities: ['Patchwork Pauper', 'Loot Master']
+        abilities: ['Patchwork Pauper', 'Loot Master'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 369
+        }
     }, {
         name: 'Assassin',
         path: 'Rogue',
-        abilities: ['First Strike', 'Deadly Poisons']
+        abilities: ['First Strike', 'Deadly Poisons'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 410
+        }
     }, {
         name: 'Shadowstalker',
         path: 'Rogue',
-        abilities: ['Shadow Speed', 'Shadow Fury']
+        abilities: ['Shadow Speed', 'Shadow Fury'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 2,
+            section: 586
+        }
     }, {
         name: 'Swordsmaster',
         path: 'Rogue',
-        abilities: ['Swift Strikes', 'Ambidextrous']
+        abilities: ['Swift Strikes', 'Ambidextrous'],
+        bookRef: {
+            book: 'The Legion of Shadow',
+            act: 3,
+            section: 611
+        }
     }
 ];
 
